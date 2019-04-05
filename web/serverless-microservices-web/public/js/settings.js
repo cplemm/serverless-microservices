82603f34b8827df0e7250929a3d0b9919118da58
Carsten Lemm <clemm@microsoft.com>
2019-04-05 09:21:40 +0200

    Create settings.js

---
+++ web/serverless-microservices-web/public/js/settings.js
@@ -0,0 +1,13 @@
+// Auth
+window.authClientId = '0e3760a8-d2ac-4e13-8026-7d9f89c5efcd';
+window.authAuthority = 'https://clemmrideshare.b2clogin.com/tfp/clemmrideshare.onmicrosoft.com/b2c_1_default-signin/v2.0';
+window.authScopes = ['https://clemmrideshare.onmicrosoft.com/api/rideshare'];
+window.authEnabled = true;
+
+// API endpoints
+window.apiKey = '96e26b2432334d2e9f45c49731c1d8d2';
+window.apiBaseUrl = 'https://clemmrideshare.azure-api.net';
+window.apiDriversBaseUrl = `${window.apiBaseUrl}/d`;
+window.apiTripsBaseUrl = `${window.apiBaseUrl}/t`;
+window.apiPassengersBaseUrl = `${window.apiBaseUrl}/p`;
+window.signalrInfoUrl = 'https://clemmrideshare.service.signalr.net/api/signalrinfo';
